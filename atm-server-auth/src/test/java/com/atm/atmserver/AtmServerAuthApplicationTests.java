03f4b2f431792156acb65b8f00d5a0e05ba3bb81
proTein <3489114926@qq.com>
2026-03-20 00:01:34 +0800

    初步数据库

---
+++ atm-server-auth/src/test/java/com/atm/atmserver/AtmServerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atm.atmserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmServerAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
